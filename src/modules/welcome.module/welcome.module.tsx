651c457edc50d30926b28e63fff0da6c52c9f8d0
uliatitova <tulia_87@mail.ru>
2022-04-10 02:11:58 +0300

    welcome block updated

--- src/modules/welcome.module/welcome.module.tsx
+++ src/modules/welcome.module/welcome.module.tsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import React, {useState} from "react";
-import {Button, Container, Stack} from "@mui/material";
+import { Button, Container, Stack } from '@mui/material';
 import {useNavigate} from "react-router-dom";
 import "./welcome.css"
 //@ts-ignore
@@ -29,10 +29,26 @@ const WelcomeModule = () => {
     const slides = [
         //менять высоту через css(!)
         <Container sx={{position:"relative"}} className={"height-nav"}>
-            <img style={{ width:"100%"}} src={welcome1}/>
-            <Typography align={"center"} variant={"h6"}>Добро пожаловать!</Typography>
-            <Typography align={"center"} variant={"body1"}>Посредством нашего сервиса Вы сможете внести вклад в повышение качества медицирны </Typography>
-            <Stack sx={{position:"absolute", bottom:"35px", left:"0px", right:"0px"}}>
+            <Stack justifyContent={ "flex-end" } direction={ "column" } height={ "90vh" }>
+                    <img style={{ width:"100%"}} src={welcome1}/>
+                    <Typography align={"center"} variant={"h5"} fontWeight={ 700 } sx={{mb: "15px"}}>Добро пожаловать!</Typography>
+                    <Typography sx={{mb: "140px"}} align={"center"} variant={"body1"}>Посредством нашего сервиса Вы сможете внести вклад в повышение качества медицины </Typography>
+
+                    <Button sx={{marginX:"15px", marginBottom:"10px", textTransform: "none"}} variant={"contained"} onClick={() => prepend()}>Далее</Button>
+                    <Button sx={{marginX:"15px", textTransform: "none"}} variant={"contained"} onClick={() => navigate("/profile")}>
+                        Пропустить
+                    </Button>
+            </Stack>
+
+        </Container>,
+
+        <Container sx={{position:"relative"}} className={"height-nav"}>
+            <Stack justifyContent={ "flex-end" } direction={ "column" } height={ "90vh" }>
+                <img style={{paddingTop:"100px", width:"100%"}} src={welcome2}/>
+                <Typography align={"center"} variant={"h5"} fontWeight={ 700 } sx={{mb: "15px"}}>Где начинается будущее?</Typography>
+                <Typography sx={{mb: "140px"}} align={"center"} variant={"body1"}>В основе нашей работы роботизированный алгоритм, который позволяет ускорить обработку данных в 2 раза*</Typography>
+                {/*<Typography sx={{width:"100%"}} align={"center"} variant={"caption"}>*на основе внутренних исследований</Typography>*/}
+
                 <Button sx={{marginX:"15px", marginBottom:"10px", textTransform: "none"}} variant={"contained"} onClick={() => prepend()}>Далее</Button>
                 <Button sx={{marginX:"15px", textTransform: "none"}} variant={"contained"} onClick={() => navigate("/profile")}>
                     Пропустить
@@ -41,22 +57,14 @@ const WelcomeModule = () => {
         </Container>,
 
         <Container sx={{position:"relative"}} className={"height-nav"}>
-            <img style={{paddingTop:"100px", width:"100%"}} src={welcome2}/>
-            <Typography align={"center"} variant={"h6"}>В основе нашей работы роботизированный алгоритм, который позволяет ускорить обработку данных в 2 раза*</Typography>
-            <Typography sx={{width:"100%"}} align={"center"} variant={"caption"}>*на основе внутренних исследований</Typography>
-            <Stack sx={{position:"absolute", bottom:"35px", left:"0px", right:"0px"}}>
-                <Button sx={{marginX:"15px", marginBottom:"10px", textTransform: "none"}} variant={"contained"} onClick={() => prepend()}>Далее</Button>
-                <Button sx={{marginX:"15px", textTransform: "none"}} variant={"contained"} onClick={() => navigate("/profile")}>
-                    Пропустить
-                </Button>
-            </Stack>
-        </Container>,
+            <Stack justifyContent={ "flex-end" } direction={ "column" } height={ "90vh" }>
+                <img style={{paddingTop:"100px", width:"100%"}} src={welcome3}/>
+                <Typography align={"center"} variant={"h5"} fontWeight={ 700 } sx={{mb: "15px"}}>Наша мотивация</Typography>
+                <Typography sx={{mb: "140px"}} align={"center"} variant={"body1"}>
+                    С начала ухудшения эпидемиологической обстановки количество жалоб в мед. учреждения возросло,
+                    а качество их обработки снизилось. Мы стремимся это изменить!
+                </Typography>
 
-        <Container sx={{position:"relative"}} className={"height-nav"}>
-            <img style={{paddingTop:"100px", width:"100%"}} src={welcome3}/>
-            <Typography align={"center"} variant={"h6"}></Typography>
-            <Typography sx={{width:"100%"}} align={"center"} variant={"caption"}>*на основе внутренних исследований</Typography>
-            <Stack sx={{position:"absolute", bottom:"35px", left:"0px", right:"0px"}}>
                 <Button sx={{marginX:"15px", textTransform: "none"}} variant={"contained"} onClick={() => navigate("/profile")} >
                     Продолжить
                 </Button>
